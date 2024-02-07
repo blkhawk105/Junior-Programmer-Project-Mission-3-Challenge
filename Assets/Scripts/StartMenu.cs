4ce8688e43f8f33bb04c375384d6a9bf827fd625
blkhawk105 <blkhawk105@gmail.com>
2024-02-06 22:16:48 -0800

    Adds a new scene for the start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+        if(SaveManager.Instance.NewPlayerName.text != "")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
